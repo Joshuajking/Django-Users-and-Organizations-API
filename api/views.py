53a80bf160c5e2b2adc6cb373348286296c66a27
Joshua King <joshuajking84@gmail.com>
2023-01-22 21:58:20 -0800

    added views for Users POST and GET

--- api/views.py
+++ api/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from django.http import JsonResponse
+from .models import Users
+from .serializers import UsersSerializer
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
 
 # Create your views here.
+
+
+@api_view(["GET", "POST"])
+def user_retrieve_info_admin(request):
+    # [GET] /api/users/ List all the users for the user organization if user is `Administrator` or `Viewer`. Must return all the user model fields. Should support search by name, email. Should support filter by phone.
+
+    if request.method == "GET":
+        users = Users.objects.all()
+        serializer = UsersSerializer(users, many=True)
+        return JsonResponse({"Users": serializer.data})
+
+    # POST /api/users/ Create an user for the organization, must set password as well. Request user must be Administrator
+    if request.method == "POST":
+        serializer = UsersSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
