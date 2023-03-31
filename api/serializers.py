fc912cdb11a307ffd922e7e1915631587cc40f68
joshuaking <jjk@fusus.com>
2023-03-31 09:01:57 -0700

    *built onto serializers.py added more fields to Meta UserProfile *added create method to override built-in password for added hashing.

--- api/serializers.py
+++ api/serializers.py
@@ -1,12 +1,28 @@
 from rest_framework import serializers
-from api.models.models import UserProfile
+from api.models.user import UserProfile
 from api.models.organization import OrganizationProfile
 
 
-class UsersSerializer(serializers.ModelSerializer):
+class UserProfileSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = UserProfile
-        fields = ["id", "name", "phone", "email", "organization", "birthdate"]
+        fields = ["id", "name", "phone", "email", "organization", "birthdate", "password"]
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Create and return a new user"""
+        user = UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
 
 
 class OrganizationSerializer(serializers.ModelSerializer):
