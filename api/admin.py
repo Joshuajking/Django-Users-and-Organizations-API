3816767566f1de1a522649538111d0316f07fb0f
joshuaking <jjk@fusus.com>
2023-03-31 09:02:59 -0700

    *various syntactic changes

--- api/admin.py
+++ api/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
-from api.models.models import UserProfile
+from api.models.user import UserProfile
 from api.models.organization import OrganizationProfile
+
 
 # Register your models here.
 admin.site.register(UserProfile)
--- api/views.py
+++ api/views.py
@@ -1,11 +1,9 @@
-from api.models.models import UserProfile
-from api.models.organization import OrganizationProfile
-from .serializers import UsersSerializer
+from api.models.user import UserProfile
+from .serializers import UserProfileSerializer
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework import status
-
-# Create your views here.
+from api import models
 
 
 @api_view(["GET", "POST"])
@@ -15,13 +13,13 @@ def user_retrieve_info_admin(request):
 
     if request.method == "GET":
         users = UserProfile.objects.all()
-        serializer = UsersSerializer(users, many=True)
+        serializer = UserProfileSerializer(users, many=True)
         return Response(serializer.data)
 
-    # POST /api/users/ Create an user for the organization, must set password as well. Request user must be
+    # POST /api/users/ Create a user for the organization, must set password as well. Request user must be
     # Administrator
     if request.method == "POST":
-        serializer = UsersSerializer(data=request.data)
+        serializer = UserProfileSerializer(data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data, status=status.HTTP_201_CREATED)
